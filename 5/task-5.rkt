603016c4f825921fe9ae9b1bf12f72b9cdbd875e
Danila Kovtun <danila.kovtunn@gmail.com>
2022-09-20 20:30:23 +0300

    little fix

--- 5/task-5.rkt
+++ 5/task-5.rkt
@@ -15,7 +15,6 @@
                     ((and (empty-tree? tree) (not (= h 0))) (cc-exit #f))
                     ((and (empty-tree? tree) (= h 0)) #t)
                     ((<= h 0) (cc-exit #f))
-                    ((and (empty-tree? (tree-left tree)) (empty-tree? (tree-right tree)) (= h 1)) #t)
                     (else (and (helper (tree-left tree) (sub1 h)) (helper (tree-right tree) (sub1 h))))
                 )
             )
